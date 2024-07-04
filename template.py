2ed9010678c9d5b607e93298101bf52a2a03f95f
sachinpjaj26 <sachinjoseph054@gmail.com>
2024-07-03 23:59:24 -0400

    folder created

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging as lg
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/predciton_pipeline.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exceptions.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements-dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiment.ipynb",
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+        lg.info(f"Creating directory: {filedir} for file: {filename}")
+    if ((not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0)):
+        with open(filepath, "w") as f:
+            pass # create empty file
+        
